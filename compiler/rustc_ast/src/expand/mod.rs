7618e787f8fea8f5cc3f82865143c5ba2d3bb3ab
Robin Moussu <moussu.robin@pm.me>
2020-11-12 11:52:57 +0100

    Extract librustdoc

--- compiler/rustc_ast/src/expand/mod.rs
+++
@@ -1,3 +0,0 @@
-//! Definitions shared by macros / syntax extensions and e.g. librustc_middle.
-
-pub mod allocator;
--- compiler/rustc_codegen_cranelift/clean_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash --verbose
-set -e
-
-rm -rf target/ build/ build_sysroot/{sysroot_src/,target/} perf.data{,.old}
-rm -rf rand/ regex/ simple-raytracer/
